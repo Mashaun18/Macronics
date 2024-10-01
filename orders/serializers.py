6979b94f120f74f2908281996d1b7cca667a1033
Mashaun18 <animashaunoreoluwa@gmail.com>
2024-10-02 00:52:41 +0100

    fix update orders views and serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,148 +1,201 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import get_object_or_404
+from .models import Order, OrderItem, Product
 from django.db import transaction
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = serializers.CharField()
+    product = serializers.CharField()  # Product identifier
 
     class Meta:
         model = OrderItem
-        fields = '__all__'
-
-    # def get_product_from_data(self, product_data):
-    #     """
-    #     This method checks if the incoming product data is either a pk, name, or slug.
-    #     """
-    #     try:
-    #         # Handle lookup by primary key (int)
-    #         if isinstance(product_data, str) and product_data.isdigit():
-    #             product = Product.objects.filter(pk=int(product_data)).first()
-    #             if not product:
-    #                 raise serializers.ValidationError(f"Product with ID '{product_data}' does not exist.")
-    #             return product
-                
-    #         # Handle lookup by unique product name
-    #         elif isinstance(product_data, str):
-    #             products = Product.objects.filter(name=product_data)
-    #             if products.count() == 1:
-    #                 return products.first()
-    #             # Handle lookup by unique slug
-    #             products = Product.objects.filter(slug=product_data)
-    #             if products.count() == 1:
-    #                 return products.first()
-            
-    #         raise serializers.ValidationError(f"Multiple or no products found with name/slug '{product_data}'")
-
-    #     except ValueError:
-    #         raise serializers.ValidationError(f"Invalid product identifier '{product_data}'. It should be an integer, name, or slug.")
-
-    #     return None
-
-# class OrderSerializer(serializers.ModelSerializer):
-#     items = OrderItemSerializer(many=True)
-
-#     class Meta:
-#         model = Order
-#         fields = '__all__'
-
-#     def create(self, validated_data):
-#         items_data = validated_data.pop('items')
-#         order = Order.objects.create(**validated_data)
-
-#         for item_data in items_data:
-#             product = self.get_product_from_data(item_data.get('product'))
-#             if product:
-#                 OrderItem.objects.create(order=order, product=product, quantity=item_data.get('quantity'))
-#         return order
-
-#     def update(self, instance, validated_data):
-#         items_data = validated_data.pop('items', None)
-
-#         # Update order fields
-#         instance.status = validated_data.get('status', instance.status)
-#         instance.shipping_address = validated_data.get('shipping_address', instance.shipping_address)
-#         instance.shipping_date = validated_data.get('shipping_date', instance.shipping_date)
-#         instance.save()
-
-#         if items_data:
-#             for item_data in items_data:
-#                 product = self.get_product_from_data(item_data.get('product'))
-
-#                 # Try to get the item by ID and update or create a new one if not found
-#                 item_id = item_data.get('id')
-#                 if item_id:
-#                     item = OrderItem.objects.filter(id=item_id, order=instance).first()
-#                     if item and product:
-#                         item.quantity = item_data.get('quantity', item.quantity)
-#                         item.product = product
-#                         item.save()
-#                 else:
-#                     if product:
-#                         OrderItem.objects.create(order=instance, product=product, quantity=item_data.get('quantity'))
-
-#         return instance
-
-#     def get_product_from_data(self, product_data):
-#         """
-#         This method checks if the incoming product data is either a pk, name, or slug.
-#         """
-#         try:
-#             # Handle lookup by primary key (int)
-#             if isinstance(product_data, str) and product_data.isdigit():
-#                 product = Product.objects.filter(pk=int(product_data)).first()
-#                 if not product:
-#                     raise serializers.ValidationError(f"Product with ID '{product_data}' does not exist.")
-#                 return product
-                
-#             # Handle lookup by unique product name
-#             elif isinstance(product_data, str):
-#                 products = Product.objects.filter(name=product_data)
-#                 if products.count() == 1:
-#                     return products.first()
-#                 # Handle lookup by unique slug
-#                 products = Product.objects.filter(slug=product_data)
-#                 if products.count() == 1:
-#                     return products.first()
-            
-#             raise serializers.ValidationError(f"Multiple or no products found with name/slug '{product_data}'")
-
-#         except ValueError:
-#             raise serializers.ValidationError(f"Invalid product identifier '{product_data}'. It should be an integer, name, or slug.")
-
-#         return None
-
-
+        fields = ['product', 'quantity', 'unit_price', 'subtotal']
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+
     class Meta:
-        model = Order 
-        fields = ['id', "created_at", "status", "user", "items", "shipping_address"]
+        model = Order
+        fields = ['id', 'created_at', 'status', 'user', 'items', 'shipping_address', 'tracking_number']
+
+class CreateOrderSerializer(serializers.Serializer):
+    items = serializers.ListField(write_only=True)  # Items will be passed during order creation
+
+    def save(self, **kwargs):
+        with transaction.atomic():
+            user = self.context["user"]
+            order = Order.objects.create(user=user, status=Order.PENDING_STATUS)
+
+            order_items = []
+            for item in self.validated_data['items']:
+                product = Product.objects.get(pk=item['product'])  # Assuming product is passed as ID
+                order_item = OrderItem(
+                    order=order,
+                    product=product,
+                    quantity=item['quantity'],
+                    unit_price=product.price
+                )
+                order_items.append(order_item)
+
+            OrderItem.objects.bulk_create(order_items)
+            return order
+
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ["status"]
+
+
+
+
+
+
+
+
+
+################################################################
+# class OrderItemSerializer(serializers.ModelSerializer):
+#     product = serializers.CharField()
+
+#     class Meta:
+#         model = OrderItem
+#         fields = '__all__'
+
+#     # def get_product_from_data(self, product_data):
+#     #     """
+#     #     This method checks if the incoming product data is either a pk, name, or slug.
+#     #     """
+#     #     try:
+#     #         # Handle lookup by primary key (int)
+#     #         if isinstance(product_data, str) and product_data.isdigit():
+#     #             product = Product.objects.filter(pk=int(product_data)).first()
+#     #             if not product:
+#     #                 raise serializers.ValidationError(f"Product with ID '{product_data}' does not exist.")
+#     #             return product
+                
+#     #         # Handle lookup by unique product name
+#     #         elif isinstance(product_data, str):
+#     #             products = Product.objects.filter(name=product_data)
+#     #             if products.count() == 1:
+#     #                 return products.first()
+#     #             # Handle lookup by unique slug
+#     #             products = Product.objects.filter(slug=product_data)
+#     #             if products.count() == 1:
+#     #                 return products.first()
+            
+#     #         raise serializers.ValidationError(f"Multiple or no products found with name/slug '{product_data}'")
+
+#     #     except ValueError:
+#     #         raise serializers.ValidationError(f"Invalid product identifier '{product_data}'. It should be an integer, name, or slug.")
+
+#     #     return None
+
+# # class OrderSerializer(serializers.ModelSerializer):
+# #     items = OrderItemSerializer(many=True)
+
+# #     class Meta:
+# #         model = Order
+# #         fields = '__all__'
+
+# #     def create(self, validated_data):
+# #         items_data = validated_data.pop('items')
+# #         order = Order.objects.create(**validated_data)
+
+# #         for item_data in items_data:
+# #             product = self.get_product_from_data(item_data.get('product'))
+# #             if product:
+# #                 OrderItem.objects.create(order=order, product=product, quantity=item_data.get('quantity'))
+# #         return order
+
+# #     def update(self, instance, validated_data):
+# #         items_data = validated_data.pop('items', None)
+
+# #         # Update order fields
+# #         instance.status = validated_data.get('status', instance.status)
+# #         instance.shipping_address = validated_data.get('shipping_address', instance.shipping_address)
+# #         instance.shipping_date = validated_data.get('shipping_date', instance.shipping_date)
+# #         instance.save()
+
+# #         if items_data:
+# #             for item_data in items_data:
+# #                 product = self.get_product_from_data(item_data.get('product'))
+
+# #                 # Try to get the item by ID and update or create a new one if not found
+# #                 item_id = item_data.get('id')
+# #                 if item_id:
+# #                     item = OrderItem.objects.filter(id=item_id, order=instance).first()
+# #                     if item and product:
+# #                         item.quantity = item_data.get('quantity', item.quantity)
+# #                         item.product = product
+# #                         item.save()
+# #                 else:
+# #                     if product:
+# #                         OrderItem.objects.create(order=instance, product=product, quantity=item_data.get('quantity'))
+
+# #         return instance
+
+# #     def get_product_from_data(self, product_data):
+# #         """
+# #         This method checks if the incoming product data is either a pk, name, or slug.
+# #         """
+# #         try:
+# #             # Handle lookup by primary key (int)
+# #             if isinstance(product_data, str) and product_data.isdigit():
+# #                 product = Product.objects.filter(pk=int(product_data)).first()
+# #                 if not product:
+# #                     raise serializers.ValidationError(f"Product with ID '{product_data}' does not exist.")
+# #                 return product
+                
+# #             # Handle lookup by unique product name
+# #             elif isinstance(product_data, str):
+# #                 products = Product.objects.filter(name=product_data)
+# #                 if products.count() == 1:
+# #                     return products.first()
+# #                 # Handle lookup by unique slug
+# #                 products = Product.objects.filter(slug=product_data)
+# #                 if products.count() == 1:
+# #                     return products.first()
+            
+# #             raise serializers.ValidationError(f"Multiple or no products found with name/slug '{product_data}'")
+
+# #         except ValueError:
+# #             raise serializers.ValidationError(f"Invalid product identifier '{product_data}'. It should be an integer, name, or slug.")
+
+# #         return None
+
+
+
+# class OrderSerializer(serializers.ModelSerializer):
+#     items = OrderItemSerializer(many=True, read_only=True)
+#     class Meta:
+#         model = Order 
+#         fields = ['id', "created_at", "status", "user", "items", "shipping_address"]
         
 
 
 
-class CreateOrderSerializer(serializers.Serializer):
+# class CreateOrderSerializer(serializers.Serializer):
     
-    def save(self, **kwargs):
-        with transaction.atomic():
-            user = self.context["user"]
-            order = Order.objects.create(user = user)
-            orderitems = [
-                OrderItem(order=order, 
-                    product=item.product, 
-                    quantity=item.quantity
-                    )
-            for item in orderitems
-            ]
-            OrderItem.objects.bulk_create(orderitems)
-            # Cart.objects.filter(id=cart_id).delete()
-            return order
+#     def save(self, **kwargs):
+#         with transaction.atomic():
+#             user = self.context["user"]
+#             order = Order.objects.create(user = user)
+#             orderitems = [
+#                 OrderItem(order=order, 
+#                     product=item.product, 
+#                     quantity=item.quantity
+#                     )
+#             for item in orderitems
+#             ]
+#             OrderItem.objects.bulk_create(orderitems)
+#             # Cart.objects.filter(id=cart_id).delete()
+#             return order
 
 
-class UpdateOrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order 
-        fields = ["pending_status"]
+# class UpdateOrderSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Order 
+#         fields = ["pending_status"]
+
+###########################
+
+
+
--- orders/views.py
+++ orders/views.py
@@ -1,66 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import viewsets, status
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.pagination import PageNumberPagination
 from .models import Order
 from .serializers import OrderSerializer, CreateOrderSerializer, UpdateOrderSerializer
 
 class OrderViewSet(ModelViewSet):
-    
     http_method_names = ["get", "patch", "post", "delete", "options", "head"]
-    
+
     def get_permissions(self):
         if self.request.method in ["PATCH", "DELETE"]:
             return [IsAdminUser()]
         return [IsAuthenticated()]
-            
-    
-    
+
     def create(self, request, *args, **kwargs):
-        serializer = CreateOrderSerializer(data=request.data, context={"user": self.request.user.id})
+        serializer = CreateOrderSerializer(data=request.data, context={"user": self.request.user})
         serializer.is_valid(raise_exception=True)
         order = serializer.save()
         serializer = OrderSerializer(order)
         return Response(serializer.data)
-        
-    
+
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return CreateOrderSerializer
         elif self.request.method == 'PATCH':
             return UpdateOrderSerializer
         return OrderSerializer
-       
-    
+
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
             return Order.objects.all()
-        return Order.objects.filter(owner=user)
-    
-
+        return Order.objects.filter(user=user)
 
 @api_view(['PATCH'])
 def update_order_status(request, pk):
     try:
-        order = Order.objects.get(pk=pk)
+        order = get_object_or_404(Order, pk=pk)
         status_value = request.data.get('status')
 
-        if not status_value:
-            return Response({'error': 'No status provided'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if status_value not in dict(Order.STATUS_CHOICES):
-            return Response({'error': 'Invalid status provided'}, status=status.HTTP_400_BAD_REQUEST)
+        if not status_value or status_value not in dict(Order.STATUS_CHOICES):
+            return Response({'error': 'Invalid or missing status'}, status=status.HTTP_400_BAD_REQUEST)
 
         order.status = status_value
         order.save()
         return Response({'message': 'Order status updated successfully'}, status=status.HTTP_200_OK)
-
-    except Order.DoesNotExist:
-        return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
 
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -68,47 +55,24 @@ def update_order_status(request, pk):
 @api_view(['GET'])
 def cart_details(request):
     user = request.user
-    cart = Order.objects.filter(user=user, status='cart').first()
+    cart = Order.objects.filter(user=user, status=Order.CART_STATUS).first()
     if not cart:
         return Response({'message': 'No active cart'}, status=status.HTTP_404_NOT_FOUND)
-    
+
     serializer = OrderSerializer(cart)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def order_history(request):
     user = request.user
-    orders = Order.objects.filter(user=user).exclude(status='cart')
+    orders = Order.objects.filter(user=user).exclude(status=Order.CART_STATUS)
 
     if not orders.exists():
         return Response({'message': 'No order history found'}, status=status.HTTP_404_NOT_FOUND)
 
-    # Use pagination
     paginator = PageNumberPagination()
-    paginator.page_size = 20  # Adjust the page size as needed
+    paginator.page_size = 20
     paginated_orders = paginator.paginate_queryset(orders, request)
 
     serializer = OrderSerializer(paginated_orders, many=True)
     return paginator.get_paginated_response(serializer.data)
-
-
-
-
-
-
-
-#    class OrderViewSet(viewsets.ModelViewSet):
-#     serializer_class = OrderSerializer
-#     permission_classes = [IsAuthenticated]
-
-#     def get_queryset(self):
-#         return Order.objects.filter(user=self.request.user)
-
-#     def perform_create(self, serializer):
-#         # Associate the order with the current user
-#         serializer.save(user=self.request.user)
-#         # Product and item creation logic is handled inside the serializer now
-
-
-    # def get_serializer_context(self):
-    #     return {"user_id": self.request.user.id}
