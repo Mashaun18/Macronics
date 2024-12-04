c66d74d4148fb9a3e50ae29cfbb876a7e84ac1f0
Mashaun18 <animashaunoreoluwa@gmail.com>
2024-12-05 00:23:21 +0100

    modify vendors admin.py

--- vendors/admin.py
+++ vendors/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from .models import Vendor
 
-# Register your models here.
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
     list_display = ('business_name', 'user', 'contact_phone', 'verified', 'created_at', 'listing_fee_paid', 'subscription_expiry', 'subscription_active')
-    search_fields = ('business_name', 'user__username', 'cac_number', 'listing_fee_paid', 'subscription_expiry', 'subscription_active')
-    list_filter = ('verified', 'created_at', 'listing_fee_paid', 'subscription_expiry', 'subscription_active')
+    search_fields = ('business_name', 'user__username', 'cac_number', 'listing_fee_paid')
+    list_filter = ('verified', 'created_at', 'listing_fee_paid', 'subscription_expiry')
     fields = ('business_name', 'user', 'cac_number', 'contact_phone', 'address', 'verified')
-
-    # Add a custom action to verify vendors
     actions = ['verify_vendors']
 
+    def subscription_active(self, obj):
+        return obj.is_subscription_active()
+    subscription_active.boolean = True
+    subscription_active.short_description = 'Subscription Active'
+
     def verify_vendors(self, request, queryset):
-        updated = queryset.update(verified=True)  # Mark selected vendors as verified
+        unverified_vendors = queryset.filter(verified=False)
+        updated = unverified_vendors.update(verified=True)
         self.message_user(request, f'{updated} vendor(s) successfully verified.')
-    verify_vendors.short_description = 'Verify selected vendors'
